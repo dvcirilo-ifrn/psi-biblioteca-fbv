5babf341506ae061b961d47343c7614b36b2b6d2
Diego Cirilo <dvcirilo@gmail.com>
2025-02-20 00:05:31 -0300

    dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,15 @@ from . import views
 app_name="dashboard"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("livros/", views.listar_livros, name="listar-livros"),
+    path("livros/criar/", views.criar_livro, name="criar-livro"),
+    path("livros/<int:id>/", views.ler_livro, name="ler-livro"),
+    path("livros/<int:id>/editar/", views.editar_livro, name="editar-livro"),
+    path("livros/<int:id>/remover/", views.remover_livro, name="remover-livro"),
+    path("usuarios/", views.listar_usuarios, name="listar-usuarios"),
+    path("usuarios/criar/", views.criar_usuario, name="criar-usuario"),
+    path("usuarios/<int:id>/", views.ler_usuario, name="ler-usuario"),
+    path("usuarios/<int:id>/editar/", views.editar_usuario, name="editar-usuario"),
+    path("usuarios/<int:id>/remover/", views.remover_usuario, name="remover-usuario"),
+    path("usuarios/<int:id>/desabilitar/", views.desabilitar_usuario, name="desabilitar-usuario"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,50 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.urls import reverse_lazy
+from contas.models import Usuario
+from biblioteca.models import Livro
 
+@login_required
+@permission_required("biblioteca.add_livro", raise_exception=True)
 def index(request):
-    return render(request, "dashboard/index.html")
+
+    context = {
+        "num_livros": Livro.objects.count(),
+        "num_usuarios": Usuario.objects.count(),
+        "num_favoritos": Livro.favoritos.through.objects.count(),
+    }
+
+    return render(request, "dashboard/index.html", context)
+
+def listar_livros(request):
+    pass
+
+def criar_livro(request):
+    pass
+
+def ler_livro(request):
+    pass
+
+def editar_livro(request):
+    pass
+
+def remover_livro(request):
+    pass
+
+def listar_usuarios(request):
+    pass
+
+def criar_usuario(request):
+    pass
+
+def ler_usuario(request):
+    pass
+
+def editar_usuario(request):
+    pass
+
+def remover_usuario(request):
+    pass
+
+def desabilitar_usuario(request):
+    pass
